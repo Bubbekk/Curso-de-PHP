d3355d429d4698b1e76acc0476ed4e69ab70bf2c
Bubbekk <jaaviergoonzalez@gmail.com>
2026-03-03 20:20:55 -0300

    Add comments to variables in variables.php
    Added comments to explain the purpose of each variable.

--- variables.php
+++ variables.php
@@ -1,10 +1,16 @@
 <?php
 
-
+// Guarda el nombre de la persona
 $nombre = "Javier";
+
+// Guarda el apellido de la persona
 $apellido = "Gonzalez";
+
+// Guarda la edad de la persona
 $edad = 30;
+
+// Guarda un mensaje de saludo
 $mensaje = "Hola";
 
+// Muestra en pantalla una oración usando las variables
 echo "Hola me llamo $nombre $apellido y tengo $edad años";
-
