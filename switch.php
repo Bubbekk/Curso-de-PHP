c4ea51b6f46120e26d46ffc3ce7ace5359ca101f
Bubbekk <jaaviergoonzalez@gmail.com>
2026-03-03 20:26:12 -0300

    Add comments to switch statement for clarity

--- switch.php
+++ switch.php
@@ -1,56 +1,72 @@
 <?php
 
+// Guarda el número del mes
 $mes = 112;
 
-switch($mes){
+// Evalúa el valor de la variable $mes
+switch ($mes) {
+
+    // Si el valor es 1, muestra Enero
     case 1:
         echo "Enero";
         break;
 
+    // Si el valor es 2, muestra Febrero
     case 2:
         echo "Febrero";
         break;
 
+    // Si el valor es 3, muestra Marzo
     case 3:
         echo "Marzo";
         break;
 
+    // Si el valor es 4, muestra Abril
     case 4:
         echo "Abril";
         break;
 
+    // Si el valor es 5, muestra Mayo
     case 5:
         echo "Mayo";
         break;
 
+    // Si el valor es 6, muestra Junio
     case 6:
         echo "Junio";
         break;
 
+    // Si el valor es 7, muestra Julio
     case 7:
         echo "Julio";
         break;
 
+    // Si el valor es 8, muestra Agosto
     case 8:
         echo "Agosto";
         break;
 
+    // Si el valor es 9, muestra Septiembre
     case 9:
         echo "Septiembre";
         break;
 
+    // Si el valor es 10, muestra Octubre
     case 10:
         echo "Octubre";
         break;
 
+    // Si el valor es 11, muestra Noviembre
     case 11:
         echo "Noviembre";
         break;
 
+    // Si el valor es 12, muestra Diciembre
     case 12:
         echo "Diciembre";
         break;
 
+    // Si no coincide con ningún caso anterior
     default:
         echo "Dato no valido";
         break;
